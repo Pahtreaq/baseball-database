9dc447c7c81a8cec19ef08cf5e4efd2ee4306ce2
mewEdge, Inc <listings@advocates-connect.com>
2019-10-30 10:36:30 +0100

    added all the queries, albeit with some problems in the creates

--- creates.sql
+++ creates.sql
@@ -7,28 +7,122 @@ USE master;
 CREATE DATABASE Baseball;
 Use Baseball;
 
-CREATE TABLE Team
+CREATE TABLE teams
 (
-    id INT
-    AUTO_INCREMENT,
-    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  id INT
+  AUTO_INCREMENT,
+  location VARCHAR
+  (255),
+  mascot VARCHAR
+  (255),
+  abbreviation VARCHAR
+  (3),
+  league ENUM
+  ('AL', 'NL') NOT NULL,
+  division ENUM
+  ('East', 'Central', 'West') NOT NULL,
+  createdAt DATETIME DEFAULT NOW
+  (),
+  updatedAt DATETIME DEFAULT NOW
+  (),
   deletedAt DATETIME,
-  PRIMARY KEY(id)
-);
-create table Player 
-(
-TeamId int auto_increment,
-	first_name VARCHAR
-    (255),
-    last_name VARCHAR
-    (255),
-  teamid INT NOT NULL,
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  deletedAt DATETIME,
-  PRIMARY KEY (memberId, productId),
-  FOREIGN KEY (memberId) REFERENCES members(id),
-  FOREIGN KEY (productId) REFERENCES products(id),
+  PRIMARY KEY
+  (id)
 );
 
+  CREATE TABLE games
+  (
+    id INT
+    AUTO_INCREMENT,
+  homeTeamId INT NOT NULL,
+  awayTeamId INT NOT NULL,
+  homeScore INT,
+  awayScore INT,
+  startTime TIMESTAMP NOT NULL,
+  createdAt DATETIME DEFAULT NOW
+    (),
+  updatedAt DATETIME DEFAULT NOW
+    (),
+  deletedAt DATETIME,
+  PRIMARY KEY
+    (id),
+  FOREIGN KEY
+    (homeTeamId) REFERENCES teams
+    (id),
+  FOREIGN KEY
+    (awayTeamId) REFERENCES teams
+    (id)
+);
+
+    CREATE TABLE players
+    (
+      id INT
+      AUTO_INCREMENT,
+  firstName VARCHAR
+      (255),
+  lastName VARCHAR
+      (255),
+  position VARCHAR
+      (10),
+  throws ENUM
+      ('R','L','B'),
+  hits ENUM
+      ('R','L','B'),
+  currentTeamId INT NOT NULL,
+  createdAt DATETIME DEFAULT NOW
+      (),
+  updatedAt DATETIME DEFAULT NOW
+      (),
+  deletedAt DATETIME,
+  PRIMARY KEY
+      (id),
+  FOREIGN KEY
+      (currentTeamId) REFERENCES teams
+      (id)
+);
+
+      CREATE TABLE hittingStats
+      (
+        playerId INT NOT NULL,
+        gameId INT NOT NULL,
+        teamId INT NOT NULL,
+        atBats INT DEFAULT 0,
+        runs INT DEFAULT 0,
+        hits INT DEFAULT 0,
+        doubles INT DEFAULT 0,
+        triples INT DEFAULT 0,
+        homeRuns INT DEFAULT 0,
+        runsBattedIn INT DEFAULT 0,
+        walks INT DEFAULT 0,
+        strikeOuts INT DEFAULT 0,
+        steals INT DEFAULT 0,
+        createdAt DATETIME DEFAULT NOW(),
+        updatedAt DATETIME DEFAULT NOW(),
+        deletedAt DATETIME,
+        PRIMARY KEY(playerId, gameId),
+        FOREIGN KEY (playerId) REFERENCES players(id),
+        FOREIGN KEY (gameId) REFERENCES games(id),
+        FOREIGN KEY (teamId) REFERENCES teams(id)
+      );
+
+      CREATE TABLE pitchingStats
+      (
+        playerId INT NOT NULL,
+        gameId INT NOT NULL,
+        teamId INT NOT NULL,
+        win TINYINT DEFAULT 0,
+        inningsPitched DECIMAL(3,1),
+        hits INT DEFAULT 0,
+        runs INT DEFAULT 0,
+        earnedRuns INT DEFAULT 0,
+        walks INT DEFAULT 0,
+        strikeOuts INT DEFAULT 0,
+        createdAt DATETIME DEFAULT NOW(),
+        updatedAt DATETIME DEFAULT NOW(),
+        deletedAt DATETIME,
+        PRIMARY KEY(playerId, gameId),
+        FOREIGN KEY (playerId) REFERENCES players(id),
+        FOREIGN KEY (gameId) REFERENCES games(id),
+        FOREIGN KEY (teamId) REFERENCES teams(id)
+      );
+ 
